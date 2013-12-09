4bf091c80ee90fc7981009e938a2cf022df92104
Sina Samangooei <sinjax@gmail.com>
2013-12-09 21:39:08 +0000

    added the menu

--- wedding/envvar.py
+++ wedding/envvar.py
@@ -1,12 +1,12 @@
 import os
 
-DEBUG = os.environ.get('DEBUG', False)
+DEBUG = os.environ.get('DEBUG', False) == "1"
 SECRET_KEY = os.environ.get('SECRECT',"notsecret")
 SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL',"sqlite:///test.db")
 
 MAIL_DEFAULT_SENDER = 'wedding@emmasinaweddingtime.info'
-MAIL_SERVER = os.environ['MAIL_SERVER']
-MAIL_USERNAME = os.environ['MAIL_USERNAME']
-MAIL_PASSWORD = os.environ['MAIL_PASSWORD']
+MAIL_SERVER = os.environ.get('MAIL_SERVER',None)
+MAIL_USERNAME = os.environ.get('MAIL_USERNAME',None)
+MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD',None)
 MAIL_PORT = 465
 MAIL_USE_SSL = True
